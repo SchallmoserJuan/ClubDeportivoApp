737d992524ce63f9ed925fcf2a6d564c2894faf4
Frost <carloslovera1314@gmaill.com>
2025-05-16 11:28:07 -0400

    pantallaEmitirCarnetCorrecionRegistroPago

--- app/src/main/java/com/example/clubdeportivoapp/RegistroPagoActivity.kt
+++ app/src/main/java/com/example/clubdeportivoapp/RegistroPagoActivity.kt
@@ -1,42 +1,20 @@
 package com.example.clubdeportivoapp
 
-import android.annotation.SuppressLint
 import android.os.Bundle
-import android.view.MotionEvent
-import android.view.View
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class RegistroPagoActivity : AppCompatActivity() {
-
-    private var dX = 0f
-    private var dY = 0f
-
-    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registro_pago)
-
-        val cardView = findViewById<View>(R.id.cardContainer)
-
-        cardView.setOnTouchListener { view, event ->
-            when (event.action) {
-                MotionEvent.ACTION_DOWN -> {
-                    // Capturamos la diferencia entre la posición del view y el toque
-                    dX = view.x - event.rawX
-                    dY = view.y - event.rawY
-                    true
-                }
-                MotionEvent.ACTION_MOVE -> {
-                    // Movemos la vista con el dedo
-                    view.animate()
-                        .x(event.rawX + dX)
-                        .y(event.rawY + dY)
-                        .setDuration(0)
-                        .start()
-                    true
-                }
-                else -> false
-            }
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_emitir_carnet_dos)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
         }
     }
 }
