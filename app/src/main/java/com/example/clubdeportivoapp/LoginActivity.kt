bff674fd00145b1c75f553eb6d1b7d1a430b98fc
rodrigochico2023 <lizzardrotiseriamonteros@gmail.com>
2025-05-17 19:41:36 -0300

    Pantallas-Inicio-Login-RecuperarContraseña

--- app/src/main/java/com/example/clubdeportivoapp/LoginActivity.kt
+++ app/src/main/java/com/example/clubdeportivoapp/LoginActivity.kt
@@ -1,31 +1,20 @@
 package com.example.clubdeportivoapp
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class InicioActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_inicio)
-
-        // Ajuste visual para que se acomode a los bordes de pantalla
+        setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        // ✅ Este es el botón que conectará a la pantalla de login
-        val btnIrAlLogin = findViewById<Button>(R.id.btnIrAlLogin)
-        btnIrAlLogin.setOnClickListener {
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
         }
     }
 }
