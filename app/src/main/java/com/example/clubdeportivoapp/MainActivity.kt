6d9add3dd9273f06320f4b605838fa2522844628
Juan Schallmoser <schallmoserjuan@gmail.com>
2025-05-17 21:41:36 -0300

    fix: errores en emulador

--- app/src/main/java/com/example/clubdeportivoapp/MainActivity.kt
+++ app/src/main/java/com/example/clubdeportivoapp/MainActivity.kt
@@ -10,7 +10,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        // Main Inicio de sesion ( Rodri )
+        setContentView(R.layout.activity_inscribir_nuevo_cliente)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
