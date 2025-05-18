1ac3c9ce1d22b283b6cc8f022b3eea37dea7e958
Juan Schallmoser <schallmoserjuan@gmail.com>
2025-05-17 22:34:41 -0300

    fix: Se borro el archivo main y reemplazo por el Inicio

--- app/src/main/java/com/example/clubdeportivoapp/LoginActivity.kt
+++ app/src/main/java/com/example/clubdeportivoapp/LoginActivity.kt
@@ -1,6 +1,8 @@
 package com.example.clubdeportivoapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,14 @@ class LoginActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
         }
+        val tvForgotPassword = findViewById<TextView>(R.id.tvForgotPassword)
+
+        tvForgotPassword.setOnClickListener {
+            val intent = Intent(this, RecuperarContrasenaActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
