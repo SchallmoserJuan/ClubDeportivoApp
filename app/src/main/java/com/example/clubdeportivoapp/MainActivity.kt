c75057c65e8e16e6a0cd93b807dc342865b66405
Frost <carloslovera1314@gmaill.com>
2025-05-17 20:34:06 -0400

    asociarPantallas

--- app/src/main/java/com/example/clubdeportivoapp/MainActivity.kt
+++ app/src/main/java/com/example/clubdeportivoapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.clubdeportivoapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btnRegistroPago = findViewById<Button>(R.id.btnRegistroPago)
+        btnRegistroPago.setOnClickListener {
+            val intent = Intent(this, RegistroPagoActivity::class.java)
+            startActivity(intent)
         }
     }
 }
